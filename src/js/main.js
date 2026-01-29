77993032dce815a4712e9eb70c630dec49446ff1
Slava Khripushin <slava.cpa@yandex.ru>
2026-01-29 21:42:35 +0700

    feat: faq

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,14 @@
-document.addEventListener('DOMContentLoaded', () => {});
+document.addEventListener('DOMContentLoaded', () => {
+  const faqItems = document.querySelectorAll('.faq-item');
+  faqItems.forEach((item) => {
+    const title = item.querySelector('.faq-item__head');
+    if (title) {
+      const content = item.querySelector('.faq-item__wrapper');
+      const text = item.querySelector('.faq-item__content');
+      item.addEventListener('click', () => {
+        item.classList.toggle('show');
+        content.style.height = item.classList.contains('show') ? `${text.scrollHeight}px` : null;
+      });
+    }
+  });
+});
